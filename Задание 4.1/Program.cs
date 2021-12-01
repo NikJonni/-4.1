6198d9ab76e1e2a15d02b0ac4d59df3b9f3404bb
Никита <@LAPTOP-CDQK0D2E>
2021-12-01 20:46:40 +0300

    Добавьте файлы проекта.

---
+++ Задание 4.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:  ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int n = 1; n <= N; n++)
+            {
+                int N1 = (n * 2) - 1;
+                int sum = n * n;
+                Console.WriteLine("Число n = {0} , сумма = {1}", N1, sum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
